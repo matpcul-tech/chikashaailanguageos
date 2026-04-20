e834405a2034fccb6f5269a55a6ac0c3b20757ae
matpcul-tech <matpcul@gmail.com>
2026-04-20 03:07:48 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import ChikashaLanguageOS from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChikashaLanguageOS />
+  </React.StrictMode>
+)
